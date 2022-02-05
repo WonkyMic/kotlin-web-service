069ebc998dbde1b26f57f6a3a7693ea020e2a5e8
wonkymic <wonkymic@gmail.com>
2022-02-05 00:42:54 -0600

    impl: rest api on localhost

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+	implementation("org.springframework.data:spring-data-jpa:2.6.1")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
--- src/main/kotlin/com/wonkymic/kotlinwebservice/KotlinWebServiceApplication.kt
+++ src/main/kotlin/com/wonkymic/kotlinwebservice/KotlinWebServiceApplication.kt
@@ -2,6 +2,9 @@ package com.wonkymic.kotlinwebservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+//import org.springframework.data.annotation.Id
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class KotlinWebServiceApplication
@@ -9,3 +12,15 @@ class KotlinWebServiceApplication
 fun main(args: Array<String>) {
 	runApplication<KotlinWebServiceApplication>(*args)
 }
+
+@RestController
+class MessageResources {
+	@GetMapping
+	fun index(): List<Message> = listOf(
+		Message("1", "Hello!"),
+		Message("2", "Bonjour!"),
+		Message("3", "Privet!"),
+	)
+}
+
+data class Message(val id: String?, val text: String)
